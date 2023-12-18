efe27473b94b85709852c1c3a540d2147fc6d084
Stian Wilhelmsen <stianwil@stud.ntnu.no>
2023-12-18 13:51:32 +0100

    Removed console logs

--- botsystem/pages/index.vue
+++ botsystem/pages/index.vue
@@ -17,19 +17,11 @@ export default {
     setup() {
         const router = useRouter();
         onMounted(() => {
-            const token = localStorage.getItem('jwtToken');
             if (token) {
                 store.isLoggedIn = true;
                 store.firstname = localStorage.getItem('firstname') || '';
                 store.lastname = localStorage.getItem('lastname') || '';
                 store.loggedInUserId = parseInt(localStorage.getItem('userId'), 10) || 0;
-
-                console.log('Store Values:');
-                console.log('isLoggedIn:', store.isLoggedIn);
-                console.log('firstname:', store.firstname);
-                console.log('lastname:', store.lastname);
-                console.log('loggedInUserId:', store.loggedInUserId);
-
                 if (store.isLoggedIn) {
                     router.push('/dashboard');
                 }
